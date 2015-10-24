f05a4ff8728ab3d0ec4ca6b021862859e1804143
Patricio Del Boca <patriciodelboca@gmail.com>
2015-10-23 21:29:49 -0300

    Lesson 5. Databases

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # Improt Flask app from the flask module
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g
 from functools import wraps
+import sqlite3
 # Create the application object
 app = Flask(__name__)
-
 app.secret_key = "my secret key"
+app.database = 'sample.db'
 
 # login required decorator
 def login_required(f):
@@ -21,8 +22,11 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-	#return "Hello, World!"
-	return render_template('index.html')
+	g.db = connect_db()
+	cur = g.db.execute('SELECT * FROM posts')
+	posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('index.html', posts=posts)
 
 @app.route('/welcome')
 def welcome():
@@ -46,6 +50,9 @@ def logout():
 	session.pop('logged_in', None)
 	flash('You have been logged out!')
 	return redirect(url_for('welcome'))
+
+def connect_db():
+	return sqlite3.connect(app.database)
 
 # start the server with the 'run()' method
 if __name__ == '__main__':

678a65df0eeeffb2bd0b03ac42c56133e7b8a9be
Patricio Del Boca <patriciodelboca@gmail.com>
2015-10-31 11:16:41 -0300

    Part 11. Session key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class BaseConfig(object):
 	DEBUG = False
-	SECRET_KEY = 'my precious'
+	SECRET_KEY = ',\xadF\x9b\xa2"\x9e\xd6\xb8\x87-&\xd7\xe4\xdb\x17\x8d\x1e`\x8c\xcdt\r\xe8'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
